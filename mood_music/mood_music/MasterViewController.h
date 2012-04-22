780bdf61e84c016e5e9fc83822c5937f18bbff11
andrewBatutin <abatutin@gmail.com>
2012-04-22 22:55:49 +0300

    music plays

--- mood_music/mood_music/MasterViewController.h
+++ mood_music/mood_music/MasterViewController.h
@@ -8,11 +8,27 @@
 
 #import <UIKit/UIKit.h>
 #import "ENAPI.h"
+#import <QuartzCore/QuartzCore.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @class DetailViewController;
 
-@interface MasterViewController : UIViewController
+@interface MasterViewController : UIViewController <MPMediaPickerControllerDelegate>
 
 @property (strong, nonatomic) DetailViewController *detailViewController;
+@property (nonatomic, retain) MPMusicPlayerController *musicPlayer;
+@property (strong, nonatomic) IBOutlet UISlider *volumeSlider;
+@property (strong, nonatomic) IBOutlet UIButton *playPauseButton;
+@property (strong, nonatomic) IBOutlet UIImageView *artworkImageView;
+
+- (IBAction)showMediaPicker:(id)sender;
+- (IBAction)volumeChanged:(id)sender;
+- (IBAction)previousSong:(id)sender;
+- (IBAction)playPause:(id)sender;
+- (IBAction)nextSong:(id)sender;
+
+- (void)registerMediaPlayerNotifications;
+- (void)handleNowPlayingItemChanged:(id)notification;
+- (void)handlePlaybackStateChanged:(id)notification;
 
 @end
--- mood_music/mood_music/MasterViewController.m
+++ mood_music/mood_music/MasterViewController.m
@@ -12,27 +12,211 @@
 @implementation MasterViewController
 
 @synthesize detailViewController;
-							
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-	self.navigationItem.leftBarButtonItem = self.editButtonItem;
+@synthesize musicPlayer;
+@synthesize volumeSlider;
+@synthesize playPauseButton;
+@synthesize artworkImageView;
 
-	UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(showDetailedView:)];
-	self.navigationItem.rightBarButtonItem = addButton;
+#pragma mark - Memory management
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+													name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification
+												  object: musicPlayer];
+	
+	[[NSNotificationCenter defaultCenter] removeObserver: self
+													name: MPMusicPlayerControllerPlaybackStateDidChangeNotification
+												  object: musicPlayer];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+													name: MPMusicPlayerControllerVolumeDidChangeNotification
+												  object: musicPlayer];
+    
+	[musicPlayer endGeneratingPlaybackNotifications];
 }
 
 - (void)viewDidUnload
 {
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+													name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification
+												  object: musicPlayer];
+	
+	[[NSNotificationCenter defaultCenter] removeObserver: self
+													name: MPMusicPlayerControllerPlaybackStateDidChangeNotification
+												  object: musicPlayer];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver: self
+													name: MPMusicPlayerControllerVolumeDidChangeNotification
+												  object: musicPlayer];
+    
+	[musicPlayer endGeneratingPlaybackNotifications];
+
+	[self setVolumeSlider:nil];
+	[self setPlayPauseButton:nil];
+	[self setArtworkImageView:nil];
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
 }
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+	UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(showMediaPicker:)];
+	self.navigationItem.leftBarButtonItem = addButton;
+
+	UIBarButtonItem *actionButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(showDetailedView:)];
+	self.navigationItem.rightBarButtonItem = actionButton;
+	
+	musicPlayer = [MPMusicPlayerController iPodMusicPlayer];
+    [volumeSlider setValue:[musicPlayer volume]];
+	
+	if ([musicPlayer playbackState] == MPMusicPlaybackStatePlaying)
+        [playPauseButton setImage:[UIImage imageNamed:@"pauseButton.png"] forState:UIControlStateNormal];
+	else
+        [playPauseButton setImage:[UIImage imageNamed:@"playButton.png"] forState:UIControlStateNormal];
+	
+    [self registerMediaPlayerNotifications];
+
+}
+
+#pragma mark - IBActions
 
 // show detailed view with mood chart
 - (IBAction)showDetailedView:(id)sender
 {
 	// not yet implemented
+}
+
+- (IBAction)volumeChanged:(id)sender 
+{
+    [musicPlayer setVolume:[volumeSlider value]];
+}
+
+- (IBAction)previousSong:(id)sender 
+{
+    [musicPlayer skipToPreviousItem];
+}
+
+- (IBAction)playPause:(id)sender 
+{
+    if ([musicPlayer playbackState] == MPMusicPlaybackStatePlaying)
+        [musicPlayer pause];
+	else 
+        [musicPlayer play];
+}
+
+- (IBAction)nextSong:(id)sender 
+{
+    [musicPlayer skipToNextItem];
+}
+
+#pragma mark - Notifications
+
+- (void)registerMediaPlayerNotifications 
+{
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    
+	[notificationCenter addObserver: self
+						   selector: @selector (handleNowPlayingItemChanged:)
+							   name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification
+							 object: musicPlayer];
+	
+	[notificationCenter addObserver: self
+						   selector: @selector (handlePlaybackStateChanged:)
+							   name: MPMusicPlayerControllerPlaybackStateDidChangeNotification
+							 object: musicPlayer];
+    
+    [notificationCenter addObserver: self
+						   selector: @selector (handleVolumeChanged:)
+							   name: MPMusicPlayerControllerVolumeDidChangeNotification
+							 object: musicPlayer];
+    
+	[musicPlayer beginGeneratingPlaybackNotifications];
+}
+
+
+- (void)handleNowPlayingItemChanged:(id)notification
+{
+   	MPMediaItem *currentItem = [musicPlayer nowPlayingItem];
+	UIImage *artworkImage = [UIImage imageNamed:@"noArtworkImage.png"];
+	MPMediaItemArtwork *artwork = [currentItem valueForProperty: MPMediaItemPropertyArtwork];
+	if (artwork) 
+		artworkImage = [artwork imageWithSize:CGSizeMake (200, 200)];
+    [artworkImageView setImage:artworkImage];
+    
+    NSString *titleString = [currentItem valueForProperty:MPMediaItemPropertyTitle];
+    if (titleString) {
+       // titleLabel.text = [NSString stringWithFormat:@"Title: %@",titleString];
+    } else {
+      //  titleLabel.text = @"Title: Unknown title";
+    }
+    
+    NSString *artistString = [currentItem valueForProperty:MPMediaItemPropertyArtist];
+    if (artistString) {
+      //  artistLabel.text = [NSString stringWithFormat:@"Artist: %@",artistString];
+    } else {
+       // artistLabel.text = @"Artist: Unknown artist";
+    }
+    
+    NSString *albumString = [currentItem valueForProperty:MPMediaItemPropertyAlbumTitle];
+    if (albumString) {
+     //   albumLabel.text = [NSString stringWithFormat:@"Album: %@",albumString];
+    } else {
+      //  albumLabel.text = @"Album: Unknown album";
+    }
+}
+
+- (void)handlePlaybackStateChanged:(id)notification 
+{
+    MPMusicPlaybackState playbackState = [musicPlayer playbackState];
+	
+	if (playbackState == MPMusicPlaybackStatePaused) 
+        [playPauseButton setImage:[UIImage imageNamed:@"playButton.png"] forState:UIControlStateNormal];
+	else if (playbackState == MPMusicPlaybackStatePlaying) 
+        [playPauseButton setImage:[UIImage imageNamed:@"pauseButton.png"] forState:UIControlStateNormal];
+	else if (playbackState == MPMusicPlaybackStateStopped) 
+	{
+        [playPauseButton setImage:[UIImage imageNamed:@"playButton.png"] forState:UIControlStateNormal];
+		[musicPlayer stop];
+	}
+}
+
+- (void)handleVolumeChanged:(id)notification 
+{
+    [volumeSlider setValue:[musicPlayer volume]];
+}
+
+#pragma mark - Media Picker
+
+- (IBAction)showMediaPicker:(id)sender
+{
+    MPMediaPickerController *mediaPicker = [[MPMediaPickerController alloc] initWithMediaTypes: MPMediaTypeAny];
+    
+    mediaPicker.delegate = self;
+    mediaPicker.allowsPickingMultipleItems = YES;
+    mediaPicker.prompt = @"Select songs to play";
+    
+    [self presentModalViewController:mediaPicker animated:YES];
+}
+
+- (void)mediaPicker:(MPMediaPickerController *)mediaPicker didPickMediaItems:(MPMediaItemCollection *) mediaItemCollection
+{
+    if (mediaItemCollection) 
+	{
+        [musicPlayer setQueueWithItemCollection:mediaItemCollection];
+        [musicPlayer play];
+    }
+    
+	[self dismissModalViewControllerAnimated: YES];
+}
+
+
+- (void)mediaPickerDidCancel:(MPMediaPickerController*) mediaPicker 
+{
+	[self dismissModalViewControllerAnimated: YES];
 }
 
 @end
